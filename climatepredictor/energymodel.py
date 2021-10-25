71ee02f41421429f6813931863c045908cca432c
Sol-sf <sol.quinn@live.co.uk>
2021-10-25 13:07:40 +0100

    #3 Albedo will no longer go below 0 or above 1

--- climatepredictor/energymodel.py
+++ climatepredictor/energymodel.py
@@ -8,7 +8,7 @@ em2 = 0.5
 sigma = 5.67e-8
 timestep = 1 #years
 length = 50 #years
-delta_albedo = 0.03
+delta_albedo = -0.03
 delta_em1 = 0.02
 delta_em2 = 0.02
 delta_Solar = 0
@@ -27,9 +27,10 @@ def solve_model(Solar,albedo,em1,em2,sigma):
 
 def solve_over_time(Solar,albedo,em1,em2,sigma,timestep,length,delta_albedo,delta_em1,delta_em2,delta_Solar):
     solutions_over_time = solve_model(Solar,albedo,em1,em2,sigma)
-    for t in range(1,length*calcs_per_timestep):
+    for t in range(1,int((length*calcs_per_timestep)/timestep)):
         Solar += delta_Solar/calcs_per_timestep
-        albedo += delta_albedo/calcs_per_timestep
+        if albedo < 1 and albedo > 0: albedo += delta_albedo/calcs_per_timestep
+        print(albedo)
         em1 += delta_em1/calcs_per_timestep
         em2 += delta_em2/calcs_per_timestep
 
@@ -40,6 +41,8 @@ def solve_over_time(Solar,albedo,em1,em2,sigma,timestep,length,delta_albedo,delt
 our_solution = solve_over_time(Solar,albedo,em1,em2,sigma,timestep,length,delta_albedo,delta_em1,delta_em2,delta_Solar)
 
 #Test plots
-# plt.figure()
-# plt.plot(our_solution[0,:])
-# plt.show()
+plt.figure()
+plt.plot(our_solution[0,:])
+plt.xlabel('Time years')
+plt.ylabel('Temerature K')
+plt.show()
