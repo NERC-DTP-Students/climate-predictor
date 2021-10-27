1856efc6249b9ff67bd1987bcf918220417e66bb
Matthew Wright <matthew.wright@env-res.ox.ac.uk>
2021-10-27 15:28:46 +0100

    #55 started config file

---
+++ climatepredictor/config.py
@@ -0,0 +1,19 @@
+"""Defines variables to be used across the program.
+"""
+co2_initial_update = 278.0
+co2_rate_update = 0.0
+cloud_initial_update = 50.0
+cloud_rate_update = 0.0
+albedo_initial_update = 0.3
+albedo_rate_update =  0.0
+epsilon1_initial_update = 0.5
+epsilon1_rate_update = 0.0
+epsilon2_initial_update = 0.5
+epsilon2_rate_update = 0.0
+solar_flux_update = 1370.0
+forest_update = 15.0
+ice_update = 15.0
+water_update = 65.0
+desert_update = 5.0
+# xaxis_update = 
+# time_interval
