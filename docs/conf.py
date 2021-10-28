2fe32e74dd788278acf1f05ec766704e4b3d1db2
kyubird <92575668+kyubird@users.noreply.github.com>
2021-10-28 11:44:02 +0100

    #81 updated readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Climate Predictor'
-copyright = '2021, Greta, Hannah, Matt, Sol, Kyu, Deborah'
-author = 'Greta, Hannah, Matt, Sol, Kyu, Deborah'
+copyright = '2021, NERC-DTP-Students'
+author = 'NERC-DTP-Students'
+
+#The full version, including alpha/beta/rc tags
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,8 +30,7 @@ author = 'Greta, Hannah, Matt, Sol, Kyu, Deborah'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
